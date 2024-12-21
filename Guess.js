e54f2044f18d6b7cc668fb2a0d747d0435edcf21
muhammaduali <muhamadali.anwarali@gmail.com>
2024-12-21 09:20:02 +0530

    Initial Commit

---
+++ Guess.js
@@ -0,0 +1,33 @@
+var num_1 = document.getElementById("num1")
+var bt = document.getElementById("btn")
+var re = document.getElementById("res")
+var sc = document.getElementById("sc")
+
+var score = 10
+var rand = Math.floor(Math.random() * 10) + 1
+
+bt.addEventListener("click", function () {
+    var num__1 = (num_1.value)
+    if (rand == num__1) {
+        sc.textContent = "Score : " + score
+        re.textContent = "You are Right!🙌"
+        alert("Congratulations,You Won!")
+    }
+    if (rand != num__1) {
+        score = score - 1
+        sc.textContent = "Score : " + score
+        re.textContent = "You are Wrong,Try Again!😔"
+
+    }
+    if (score == 0) {
+        re.textContent = "You Lost!😭"
+    }
+    if (score < 0) {
+        re.textContent = "Game Over!";
+        sc.style.display = "none";
+        bt.style.backgroundColor = '#BA9B45'; // Retain the background color (or keep it as you want)
+        bt.style.border = 'solid #BA9B45 2px'; // Retain the border style
+        bt.style.cursor = 'default';
+
+    }
+})
